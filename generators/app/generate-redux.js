5b7fdc2f9e6a169f17dc01a91f861a7db74267b7
Ricardo Manoel Junior <rmanoel@brdlrmanoel.daitan.com>
2019-03-12 16:09:41 -0300

    dev: controller with appId interpolation

--- generators/app/generate-redux.js
+++ generators/app/generate-redux.js
@@ -30,6 +30,38 @@ class GenerateRedux {
     )
   }
 
+  writingWithReduxAppJS() {
+    this.generator.fs.copyTpl(
+      this.generator.templatePath('src/javascript/app.js'),
+      this.generator.destinationPath('src/javascript/app.js'),
+      {
+        imports:`
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from '../store/store-config';
+import Routes from '../routes/routes';'
+        `,
+        reactDOM: `
+        const store = configureStore();
+
+        ReactDOM.render(
+          <Provider store={store}>
+            <div>
+              <Routes
+                userId={userId}
+                jwtService={extendedUserInfoService}
+              />
+            </div>
+          </Provider>,
+          document.getElementById('root'),
+        );`,
+        appName: this.generator.answers.name,
+        appId: this.generator.answers.appId,
+    },
+    );
+  }
+
 }
 
 module.exports = GenerateRedux;
--- generators/app/templates/mock-js/controller-mock.js
+++ generators/app/templates/mock-js/controller-mock.js
@@ -1,42 +1,43 @@
 import GeneralEnricher from '../src/services/general-enricher';
 
 /* global SYMPHONY */
-const templateControllerService = SYMPHONY.services.register('template:controller');
-SYMPHONY.services.register('template:enricher');
+
+const appId = <%- appId %>;
+
+const controllerService = SYMPHONY.services.register(`${appId}:controller`);
+SYMPHONY.services.register(`${appId}:enricher`);
 
 SYMPHONY.remote.hello().then(() => {
   SYMPHONY.application.register(
-    'template',
+    `${appId}`,
     ['modules', 'applications-nav', 'ui', 'entity'],
-    ['template:controller', 'template:enricher'],
+    [`${appId}:controller`, `${appId}:enricher`],
   ).then(() => {
     const modulesService = SYMPHONY.services.subscribe('modules');
     const navService = SYMPHONY.services.subscribe('applications-nav');
     const uiService = SYMPHONY.services.subscribe('ui');
     SYMPHONY.services.subscribe('entity');
-    const enricher = new GeneralEnricher('template:enricher', [
-      'org.symphony.ms.devtools.template.page',
-      'org.symphony.ms.devtools.template.blogpost',
-      'org.symphony.ms.devtools.template.loginResponse',
+    const enricher = new GeneralEnricher(`${appId}:enricher`, [
+      'org.symphony.ms.devtools.myEntity',
     ]);
 
     enricher.init();
     enricher.register();
 
-    uiService.registerExtension('app-settings', 'template', 'template:controller', { label: 'Configure Application!' });
+    uiService.registerExtension('app-settings', `${appId}`, `${appId}:controller`, { label: 'Configure Application!' });
 
     // FOR USE ON ROUTES
-    templateControllerService.implement({
+    controllerService.implement({
       trigger() {
         const configUrl = `https://${window.location.host}/app.html?queryObj={"page": "config"}`;
 
         modulesService.show(
-          'template',
+          `${appId}`,
           {
             title: 'Template',
             icon: 'https://localhost:4000/assets/app-icon.svg',
           },
-          'template:controller',
+          `${appId}:controller`,
           configUrl,
           { canFloat: true },
         );
@@ -44,28 +45,28 @@ SYMPHONY.remote.hello().then(() => {
     });
 
     const navSettings = {
-      title: 'Template',
+      title: `${appId}`,
       icon: 'https://localhost:4000/assets/app-icon.svg',
     };
-    navService.add('template-nav', navSettings, 'template:controller');
-    templateControllerService.implement({
+    navService.add(`${appId}-nav`, navSettings, `${appId}:controller`);
+    controllerService.implement({
       select(id) {
-        if (id === 'template-nav') {
-          navService.focus('template-nav');
+        if (id === `${appId}-nav`) {
+          navService.focus(`${appId}-nav`);
         }
         modulesService.show(
-          'template',
+          `${appId}`,
           {
-            title: 'Template',
+            title: `${appId}`,
             icon: 'https://localhost:4000/assets/app-icon.svg',
           },
-          'template:controller',
+          `${appId}:controller`,
           'https://localhost:4000/app.html',
           {
             canFloat: true,
           },
         );
-        modulesService.focus('template');
+        modulesService.focus(`${appId}`);
       },
     });
   });
