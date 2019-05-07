80865ada89de716f7ba4800a8078691fc3fa9b49
Ricardo Manoel Junior <rdomanoel@gmail.com>
2019-05-07 16:42:19 -0300

    refact: rename bundle.json information to Template App

--- src/utils/app-constants.js
+++ src/utils/app-constants.js
@@ -1,3 +1,3 @@
-export const APP_ID = 'testapp';
-export const APP_TITLE = 'TESTING GitLab Integration';
-export const APP_NAV_BAR_TITLE = 'TESTING GitLab Integration';
+export const APP_ID = 'templateapp';
+export const APP_TITLE = 'Template for app Integration';
+export const APP_NAV_BAR_TITLE = 'Template for app Integration';
